84693e8c0efb561a99abe17eb26db9a08b78b192
syouon <syouon.do@gmail.com>
2016-01-10 22:40:01 +0100

    Basic adding task feature (without persistence)

--- app/components/main.js
+++ app/components/main.js
@@ -2,49 +2,83 @@
  * Created by syouon.
  */
 
-var Title = React.createClass({
-    render: function () {
-        return (
-            <h1>{this.props.text}</h1>
-        );
-    }
-});
+var data = [
+    {id: 0, label: "Task number 1"},
+    {id: 1, label: "Task number 2"},
+    {id: 2, label: "Task number 3"}
+];
 
-var TodoInput = React.createClass({
-    render: function () {
-        return <input type="text" placeholder="What am I going to do?"/>;
-    }
-});
+var TodoPage = React.createClass({
+    getInitialState: function () {
+        return {
+            freeId: 3,
+            data: this.props.data
+        };
+    },
 
-var TodoValidButton = React.createClass({
-    render: function () {
-        return <input type="submit" value={this.props.text}/>;
-    }
-});
+    onAddingTask: function (label) {
+        data.push({ id: this.state.freeId, label: label});
+        var newFreeId = this.state.freeId+1;
+        var newState = {
+            freeId: newFreeId,
+            data: data
+        };
+        this.setState(newState);
+    },
 
-var TodoForm = React.createClass({
-    render: function () {
-        return (
-            <form>
-                <TodoInput/>
-                <TodoValidButton text="Add"/>
-            </form>
-        );
-    }
-});
-
-var TodoHeader = React.createClass({
     render: function () {
         return (
             <div>
-                <Title text="Todo list"/>
-                <TodoForm />
+                <h1>Todo list</h1>
+                <TodoForm onSubmit={this.onAddingTask} />
+                <TodoList data={this.state.data} />
             </div>
         );
     }
 });
 
+var TodoForm = React.createClass({
+    getInitialState: function () {
+        return { task: '' };
+    },
+
+    submitting: function () {
+        this.props.onSubmit(this.state.task);
+    },
+
+    onInputChange: function (e) {
+        this.setState({ task: e.target.value});
+    },
+
+    render: function () {
+        return (
+            <form onSubmit={this.submitting}>
+                <input type="text" placeholder="What's next?" value={this.state.task} onChange={this.onInputChange} />
+                <input type="submit" />
+            </form>
+        );
+    }
+});
+
+var TodoList = React.createClass({
+
+    render: function () {
+        var tasks = this.props.data.map(function (task) {
+            return <TodoTask key={task.id}>{task.label}</TodoTask>;
+        });
+        return <ul>{tasks}</ul>;
+    }
+});
+
+var TodoTask = React.createClass({
+    render: function () {
+        return (
+            <li>{this.props.children}</li>
+        );
+    }
+});
+
 ReactDOM.render(
-    <TodoHeader />,
+    <TodoPage data={data} />,
     document.getElementById('content')
 );
